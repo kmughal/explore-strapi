cead4f415768380cd9fd710b14f43e367cd75cca
Khurram <khurramshahzad@tfl.gov.uk>
2020-06-24 11:52:32 +0100

    override the default controller

--- api/todo/controllers/todo.js
+++ api/todo/controllers/todo.js
@@ -1,8 +1,18 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  create: async (ctx) => {
+    const input = ctx.request.body;
+    if (ctx.state.user) input.user = ctx.state.user;
+    await strapi.services.todo.create(ctx.request.body);
+  },
+  find: async (ctx) => {
+    const result = await strapi.query("todo").find({ user: ctx.state.user.id });
+    return result;
+  },
+};
